dc40f47db8dd3343178f8d3645557223c7ffac6d
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-23 16:43:00 -0600

    Wrapp app component into a Provider

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './components/app';
+import store from '../src/redux/store';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
