97a328964407ccb3f276e7631a475f4022a31f6d
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-23 15:09:49 -0600

    Creeate a root for the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  root 'root#index'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # get "up" => "rails/health#show", as: :rails_health_check
 end
